a93edbb4d1444562aa69d7b5bbb3f516f59c3ca8
HyunSJ <dudtod1596@gmail.com>
2024-03-20 12:11:57 +0900

    fix: 스크립트 로딩 개선

--- src/app/(cart)/layout.tsx
+++ src/app/(cart)/layout.tsx
@@ -1,8 +1,13 @@
+import Script from 'next/script'
+
 function layout({ children }: { children: React.ReactNode }) {
   return (
-    <main className="max-w-[1024px] flex flex-col bg-white items-center text-grayscale-700 mx-auto h-[100dvh] divide-y-8 divide-grayscale-50">
-      {children}
-    </main>
+    <>
+      <main className="mx-auto flex h-[100dvh] max-w-[1024px] flex-col items-center divide-y-8 divide-grayscale-50 bg-white text-grayscale-700">
+        {children}
+      </main>
+      <Script src="https://cdn.iamport.kr/v1/iamport.js" />
+    </>
   )
 }
 
